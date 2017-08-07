09c77c1d91fef652ce8a969beaed62ded9805e14
admin <admin@LOVEUDAD>
2017-08-07 23:40:58 +0530

    First commit

---
+++ src/corejava/Xpattern.java
@@ -0,0 +1,56 @@
+package corejava;
+
+public class Xpattern {
+public static void main(String[] args) {
+		   int n=3,i,j;
+		   for(i=n; i>=1; i--){
+			   for(j=i; j<n; j++){
+				   System.out.print(" ");
+			   }
+			   
+			   for(j=1; j<=(2*i-1); j++)
+		        {
+		            if(j==1 || j==(2*i-1))
+		            {
+		                System.out.print("*");
+		            }
+		            else
+		            {
+		            	System.out.print(" ");
+		            }
+		        }
+		 
+			   System.out.println();
+		    }
+		 
+		   for(i=2; i<=n; i++)
+		    {
+		        //Prints trailing spaces
+		        for(j=i; j<n; j++)
+		        {
+		        	System.out.print(" ");
+		        }
+		 
+		        //Prints lower hollow triangular part
+		        for(j=1; j<=(2*i-1); j++)
+		        {
+		            if(j==1 || j==(2*i-1))
+		            {
+		            	System.out.print("*");
+		            }
+		            else
+		            {
+		            	System.out.print(" ");
+		            }
+		        }
+		 
+		        System.out.println();
+		    }
+		   
+		   }
+		}
+
+
+
+
+
